532fb289d62f8262717d3b96498511db7a0d5ac4
Natarajan <medianatraaj@gmail.com>
2019-01-29 14:44:46 +0530

    description

---
+++ Monitor.ps1
@@ -0,0 +1,2 @@
+﻿$result = Start-Process -FilePath C:\Users\Natarajan\Desktop\curl-7.62.0-win64-mingw\bin\curl.exe -ArgumentList '-s -o /dev/null -I -w "%{http_code}" http://www.example.org/' | cmd
+$result
